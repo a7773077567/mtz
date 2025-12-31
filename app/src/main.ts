d37c49f737233333814a6ef9b3e66aa7d7e38e7c
AJay <andrewli707577@gmail.com>
2025-12-31 17:53:28 +0800

    feat: 講日期選擇器改為中文

--- app/src/main.ts
+++ app/src/main.ts
@@ -14,9 +14,22 @@ app.use(PrimeVue, {
     options: {
       darkModeSelector: '.dark-mode',
     }
+  },
+  locale: {
+    firstDayOfWeek: 0,
+    dayNames: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+    dayNamesShort: ['日', '一', '二', '三', '四', '五', '六'],
+    dayNamesMin: ['日', '一', '二', '三', '四', '五', '六'],
+    monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    today: '今天',
+    clear: '清除',
+    dateFormat: 'yy-mm-dd',
+    weekHeader: '週'
   }
 })
 app.use(ToastService)
 
 app.use(router)
 app.mount('#app')
+
